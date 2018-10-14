257f2142ebcb8b143d641465af3acd77abdc715d
david <david.satiricon@gmail.com>
2018-10-14 12:40:01 -0300

    Añado media commands

--- web/modules/yuki/src/Commands/MediaCommands.php
+++ web/modules/yuki/src/Commands/MediaCommands.php
@@ -2,7 +2,39 @@
 
 namespace Drupal\yuki\Commands;
 
+use Drupal\Core\Entity\Sql\SqlEntityStorageInterface;
+use Drupal\file\FileStorageInterface;
+
 class MediaCommands
 {
+
+
+  /** @var FileStorageInterface */
+  private $fileStorage;
+
+  /** @var  */
+  private $mediaStorage;
+
+  /**
+   * @command yuki:media:albums
+   * @aliases yual
+   *
+   */
+  public function createAlbums()
+  {
+    $this->fileStorage;
+
+  }
+
+
+  public function setFileStorage(FileStorageInterface $fileStorage) {
+
+    $this->fileStorage = $fileStorage;
+  }
+
+  public function setMediaStorage(SqlEntityStorageInterface $mediaStorage) {
+
+    $this->mediaStorage = $mediaStorage;
+  }
 
 }
