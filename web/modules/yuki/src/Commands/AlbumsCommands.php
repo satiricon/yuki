08e28cc8c51d05bf1f1682bf08097080b4891420
David <david.satiricon@gmail.com>
2019-11-26 19:30:29 -0500

    I add Artis to Albums and Albums to Artists

--- web/modules/yuki/src/Commands/AlbumsCommands.php
+++ web/modules/yuki/src/Commands/AlbumsCommands.php
@@ -39,14 +39,14 @@ class AlbumsCommands
 
       $title = $song->get('field_album')->value;
 
-      if(empty($title)){
+      if (empty($title)) {
         $title = 'Unknown';
       }
 
       $node = array_shift($this->nodeStorage->loadByProperties(
         ['title' => $title]));
 
-      if(!$node) {
+      if (!$node) {
         $values = [
           'type' => 'album',
           'title' => $title
@@ -56,27 +56,79 @@ class AlbumsCommands
 
       }
 
+
+      //I add a reference to an artist/s to the album
+      /*$artist = $song->get('field_artist')->value;
+      dump($artist);
+      $artists = $node->get('field_artist')->getValue();
+      dump($artists);
+      $artists[] = $artist;
+      $node->set('field_artist', $artists);*/
+
       $values = $node->get('field_song')->getValue();
       $values[] = $song;
       $node->set('field_song', $values);
 
       $node->save();
+
+      $this->addArtist($song, $node);
+    }
+
+  }
+
+  private function addArtist($song, $album)
+  {
+    $artistName = $song->get('field_artist')->value;
+
+    $artistNode = array_shift($this->nodeStorage->loadByProperties(
+      ['title' => $artistName]));
+
+    if (!$artistNode) {
+      $values = [
+        'type' => 'artist',
+        'title' => $artistName
+      ];
+      $artistNode = $this->nodeStorage->create($values);
+    }
+    $artistNode->save();
+
+    $artists = $album->get('field_artist')->referencedEntities();
+
+    $exists = array_filter($artists, function ($artist) use ($artistNode) {
+      if ($artist->id() === $artistNode->id()) {
+        return true;
+      }
+      return false;
+    });
+
+    if (!$exists) {
+      $artists[] = $artistNode;
+      $album->set('field_artist', $artists);
+      $album->save();
+
+      $artistAlbums = $artistNode->get('field_albums')->referencedEntities();
+      $artistAlbums[] = $album;
+      $artistNode->set('field_albums', $artistAlbums);
+      $artistNode->save();
     }
 
   }
 
 
-  public function setFileStorage(FileStorageInterface $fileStorage) {
+  public function setFileStorage(FileStorageInterface $fileStorage)
+  {
 
     $this->fileStorage = $fileStorage;
   }
 
-  public function setMediaStorage(SqlEntityStorageInterface $mediaStorage) {
+  public function setMediaStorage(SqlEntityStorageInterface $mediaStorage)
+  {
 
     $this->mediaStorage = $mediaStorage;
   }
 
-  public function setNodeStorage(SqlEntityStorageInterface $nodeStorage) {
+  public function setNodeStorage(SqlEntityStorageInterface $nodeStorage)
+  {
 
     $this->nodeStorage = $nodeStorage;
   }
